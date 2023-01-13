4e206667fc789f8ac1f75369937a58723f30f0aa
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-13 16:10:23 +0000

    function to remove start-screen to begin quiz.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,7 @@
 // Set variables
 
     // target start button
+let startPage = document.getElementById('start-screen');
 let startButton = document.querySelector('#start');
     // target timer
 let timerElement = document.querySelector('#time');
@@ -12,21 +13,28 @@ let timeCounter = 75;
 
 // When start button is clicked,
 function startQuiz() {
-    // set timer
     
-
+    // hides start-screen page and button.
+    hideStartScreen()
+    // start timer
     startTimer();
 }
 
     // #start-screen is hidden and #start button removed.
+
+    // fuction to hide #start button and #start-screen
+function hideStartScreen() {
+    startPage.style.display = 'none';
+}
     
     // timer starts counting.
 function startTimer() {
    let timer = setInterval(function() {
-        // decrease timer everytime
+        // decrease timer by one while counting down by one
         timeCounter--;
         timerElement.textContent = timeCounter;
         if(timeCounter === 0) {
+        // stops timer counting down when 0 is reached
             clearInterval(timer);
         }
         
