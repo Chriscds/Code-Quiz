b1eb9613480dee585cbb706b4838c29b56db40cd
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-15 17:50:23 +0000

    changed quizQuestions structure in questions.js file.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -7,37 +7,41 @@ let answerChoices = document.querySelector("#li-questions");
 let quizQuestions = [{
 
         question: "1. Which is correct about the Javascript language?",
-        answers: [
+        answers:[
                     "is an object-based language", // ✅
                     "is Assembly-language", // ❌
                     "is an Object-Oriented language", // ❌
                     "is a High-level language" // ❌
-        ],
-
+                ],
+    },
+    {
         question: "2. Arrays in JavaScript are defined by which of the following statements?",
         answers: [
                     "It is an ordered list of values",  // ✅
                     "is Assembly-language", // ❌
                     "It is an ordered list of string", // ❌
                     "It is an ordered list of functions" // ❌
-        ],
-
+                ],
+    },
+    {
         question: "3. Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
         answers: [
                     "Position", // ❌
                     "Window", // ✅
                     "Standard", // ❌
                     "Location" // ❌
-        ],
-
+                ],
+    },
+    {
         question: "4. Which of the following can be used to call a JavaScript Code Snippet?",
         answers: [
                     "Function/Method", //✅
                     "Preprocessor", // ❌
                     "Triggering Event", // ❌
                     "HTML" // ❌
-        ],
-
+                ],
+    },
+    {
         question: " Which of the following methods/operation does javascript use instead of == and !=?",
         answers: [
                     "JavaScript uses equalto()", // ❌
@@ -45,7 +49,6 @@ let quizQuestions = [{
                     "JavaScript uses bitwise checking", // ❌
                     "JavaScript uses === and !== instead" //✅
         ],
-
 }];
 
 // TODO: finish writing function to push question 1 to question-title on html page.
